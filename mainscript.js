a6349197ba680290e781e36a0a5583a4b251482b
picklechiplover23 <boblyatfunny9212@gmail.com>
2026-05-11 23:38:27 -0500

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -26,7 +26,7 @@ const AI_MODELS = [
   "mixtral-8x7b-32768",
   "gemma2-9b-it",
 ];
-const AI_ENDPOINT = "http://localhost:3000/ai";
+const AI_ENDPOINT = "https://sfools-stuff-342b8fbbb030.herokuapp.com/ai";
 const rootLink =
   "https://cdn.jsdelivr.net/gh/picklechiplover23/htmlgames@master/";
 const directories = ["root", "games", "gooner", "chat"];
